c5b392c34a707db2bd3f5b16afa956ebcf35a78e
Christopher <cefird86@gmail.com>
2023-03-18 22:19:54 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About ChatGPT</title>
+	<style type="text/css">
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+			margin: 0;
+			padding: 0;
+		}
+
+		.container {
+			max-width: 960px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0,0,0,0.2);
+			text-align: center;
+		}
+
+		.header {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+		}
+
+		.footer {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="header">
+		<h1>About ChatGPT</h1>
+	</div>
+
+	<div class="container">
+		<p>ChatGPT is a language model created by OpenAI that uses deep learning algorithms to generate human-like responses to natural language prompts. It can be used for a variety of tasks, including generating text, answering questions, and creating websites.</p>
+		<p>Using ChatGPT to create a website is easy - simply provide prompts for the content and design elements you want to include, and ChatGPT will generate the code for you. This allows you to create a fully functional website without needing to know how to code.</p>
+		<p>Of course, there are limitations to what ChatGPT can do - it may not be able to create highly complex websites or implement advanced functionality. However, for simple websites or as a starting point for more complex projects, ChatGPT can be a useful tool.</p>
+	</div>
+
+	<div class="footer">
+		<p>&copy; 2023 ChatGPT. All rights reserved.</p>
+	</div>
+</body>
+</html>
