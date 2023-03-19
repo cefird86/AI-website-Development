d8fe5d1fa8e3e5d971cbc00309e9fb43f6f65dd0
Christopher <cefird86@gmail.com>
2023-03-18 22:19:28 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ChatGPT Website</title>
+	<style type="text/css">
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+			margin: 0;
+			padding: 0;
+		}
+
+		.container {
+			max-width: 960px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0,0,0,0.2);
+			text-align: center;
+		}
+
+		.header {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+		}
+
+		.footer {
+			background-color: #333;
+			color: #fff;
+			padding: 10px;
+			text-align: center;
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="header">
+		<h1>ChatGPT Website</h1>
+	</div>
+
+	<div class="container">
+		<p>This website was created with ChatGPT.</p>
+	</div>
+
+	<div class="footer">
+		<p>&copy; 2023 ChatGPT. All rights reserved.</p>
+	</div>
+</body>
+</html>
